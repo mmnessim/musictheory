0e0d249aac8df3f99e991fe2ec72c2e4c13a9de0
Mounir Nessim <mmnessim@gmail.com>
2026-04-23 08:37:01 -0400

    Add order of sharps and flats, begin getting ready for key signature primitives

---
+++ src/key.ts
@@ -0,0 +1,21 @@
+import type { PitchClass } from "./intervals";
+
+export const SharpsOrder: PitchClass[] = [
+  "C#",
+  "F#",
+  "G#",
+  "D#",
+  "A#",
+  "E#",
+  "B#",
+];
+
+export const FlatsOrder: PitchClass[] = [
+  "Bb",
+  "Eb",
+  "Ab",
+  "Db",
+  "Gb",
+  "Cb",
+  "Fb",
+];
